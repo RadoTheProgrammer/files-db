c862840095f908cdcfb1b444798d8dbaef3ea470
RadoTheProgrammer <rrrado15@gmail.com>
2025-07-09 17:09:25 +0200

    big switch to subclass, no more monkey patching

--- main.py
+++ main.py
@@ -22,19 +22,24 @@ st_ctime 2025-02-07 22:01:40.297824
 """
 system = platform.system()
 
-if system=="Windows":
 
-    def _get_ctime(st,path):
-        return st.st_ctime
-elif system=="Darwin":
-    def _get_ctime(st,path):
+
+def _get_ctime_windows(st,path):
+    return st.st_ctime
+def _get_ctime_darwin(st,path):
         return st.st_birthtime
+def _get_ctime_linux(st,path):
+    try:
+        return subprocess.check_output(['stat', '-c', '%W', st,path]).decode().strip()
+    except Exception:
+        return None
+    
+if system=="Windows":
+    _get_ctime = _get_ctime_windows
+elif system=="Darwin":
+    _get_ctime = _get_ctime_darwin
 elif system=="Linux":
-    def _get_ctime(st,path):
-        try:
-            return subprocess.check_output(['stat', '-c', '%W', st,path]).decode().strip()
-        except Exception:
-            return None
+    _get_ctime = _get_ctime_linux
     
 
 # def get_stats(file):
@@ -63,104 +68,107 @@ ASTYPE = {
         "level":"int64",
         "nls":"int64"
         }
-def create(src,show_progression=True):
-    #print(cls)
+class FilesDatabase(pd.DataFrame):
+
+    @classmethod
+    def create(cls,src,show_progression=True):
+        #print(cls)
 
 
-    #self = self[::-1]
-    #print(type(self))
+        #self = self[::-1]
+        #print(type(self))
 
-    def _process(relpath,path,level):
-        """
-        This is the core function of the whole program, the time the program takes depend on it
-        so I optimize it as possible
-        """
-        if os.path.islink(path): # on va éviter de se casser la tête avec ces links
-            return (0,0)
+        def _process(relpath,path,level):
+            """
+            This is the core function of the whole program, the time the program takes depend on it
+            so I optimize it as possible
+            """
+            if os.path.islink(path): # on va éviter de se casser la tête avec ces links
+                return (0,0)
 
-        st=os.stat(path)
+            st=os.stat(path)
 
-        size=st.st_size
-        n=1
+            size=st.st_size
+            n=1
+            
+            if stat.S_ISDIR(st.st_mode): # equivalent to isdir
+                levelitem = level+1
+                ls = os.listdir(path)
+                for item in ls:
+                    size_add,n_add = _process(os.path.join(relpath,item),os.path.join(path,item),levelitem)
+                    size+=size_add
+                    n+=n_add
+                    pbar.update(1)
+                nls = len(ls)
+            else:
+                nls = -1
+
+            data["name"].append(relpath)
+            data["size"].append(size)
+            data["n"].append(n)
+            data["ctime"].append(_get_ctime(st,path))
+            data["mtime"].append(st.st_mtime)
+            data["atime"].append(st.st_atime)
+            data["level"].append(level)
+            data["nls"].append(nls)
+
+            return size,n
         
-        if stat.S_ISDIR(st.st_mode): # equivalent to isdir
-            levelitem = level+1
-            ls = os.listdir(path)
-            for item in ls:
-                size_add,n_add = _process(os.path.join(relpath,item),os.path.join(path,item),levelitem)
-                size+=size_add
-                n+=n_add
-                pbar.update(1)
-            nls = len(ls)
+        # end of the core function
+        
+        
+        if show_progression:
+            #print("Calculating total for progression...")
+            total = sum(map(lambda d:len(d[1])+len(d[2]),os.walk(src)))
+
         else:
-            nls = -1
+            total=0
+        
+        data={"name":[],"size":[],"n":[],"ctime":[],"mtime":[],"atime":[],"level":[], "nls":[]}
+        with tqdm(total=total,disable=not show_progression) as pbar:
+            _process("",src,0)
+        
+        self=cls(cls(data)[::-1].reset_index(drop=True).astype(ASTYPE))
+        self._remove_blank_name()
+        return self
+    @classmethod
+    def read_csv(cls,file):
+        self = cls(pd.read_csv(file).astype(ASTYPE))
+        self._remove_blank_name()
+        return self
 
-        data["name"].append(relpath)
-        data["size"].append(size)
-        data["n"].append(n)
-        data["ctime"].append(_get_ctime(st,path))
-        data["mtime"].append(st.st_mtime)
-        data["atime"].append(st.st_atime)
-        data["level"].append(level)
-        data["nls"].append(nls)
+    def ls(self):
+        return self[self["level"]==1]
 
-        return size,n
+    def _remove_blank_name(self):
+        self.name=self.name.map(lambda name:name if name else ".")
+
+    def __call__(self,item:str): # at start used __getitem__ but it would cause conflict
+        item = item.strip("/")
+        if item not in self.name.values:
+            print(".git" in self.name)
+            print("__pycache__" in self.name)
+            raise FileNotFoundError(item)
+        
+        self = self[(self.name == item) | (self.name.str.startswith(f"{item}{os.sep}"))]
+        self.level-=1
+        self.name = self.name.str[len(item)+1:] #remove the head
+        self._remove_blank_name(self)
+        return self
+
+    def pin_columns(self,*cols_to_pin):
+        cols = list(self.columns)
+        for n,col in enumerate(cols_to_pin):
+            #n+=1 # if name is a normal column
+            cols.insert(n,col)
+            cols.remove(col)
+        return self[cols]
+
+    def only_dirs(self):
+        return self[self["nls"]!=-1]
     
-    # end of the core function
-    
-    
-    if show_progression:
-        #print("Calculating total for progression...")
-        total = sum(map(lambda d:len(d[1])+len(d[2]),os.walk(src)))
-
-    else:
-        total=0
-    
-    data={"name":[],"size":[],"n":[],"ctime":[],"mtime":[],"atime":[],"level":[], "nls":[]}
-    with tqdm(total=total,disable=not show_progression) as pbar:
-        _process("",src,0)
-    
-    db=pd.DataFrame(data)[::-1].reset_index(drop=True).astype(ASTYPE)
-    _remove_blank_name(db)
-    return db
-
-def read_csv(file):
-    db = pd.read_csv(file).astype(ASTYPE)
-    _remove_blank_name(db)
-    return db
-
-pd.DataFrame.ls = lambda self:self[self["level"]==1] # monkey patching
-
-def _remove_blank_name(db):
-    db.name=db.name.map(lambda name:name if name else ".")
-
-def _call(self,item:str): # at start used __getitem__ but it would cause conflict
-    item = item.strip("/")
-    if item not in self.name.values:
-        print(".git" in self.name)
-        print("__pycache__" in self.name)
-        raise FileNotFoundError(item)
-    
-    self = self[(self.name == item) | (self.name.str.startswith(f"{item}{os.sep}"))]
-    self.level-=1
-    self.name = self.name.str[len(item)+1:] #remove the head
-    _remove_blank_name(self)
-    return self
-
-def pin_columns(self,*cols_to_pin):
-    cols = list(self.columns)
-    for n,col in enumerate(cols_to_pin):
-        #n+=1 # if name is a normal column
-        col_index = self.columns.get_loc(col)
-        cols[n], cols[col_index] = cols[col_index], cols[n]
-    return self[cols]
-
-# monkey patching
-pd.DataFrame.ls = lambda self:self[self["level"]==1] # monkey patching
-pd.DataFrame.__call__=_call
-pd.DataFrame.pin_columns=pin_columns
-pd.DataFrame.only_dirs = lambda self:self[self["nls"]!=-1]
-pd.DataFrame.only_files = lambda self:self[self["nls"]==-1]
+    def only_files(self):
+        return self[self["nls"]==-1]
 
                     
             
